a7031b12fc9c5198e96a9be9ce9f884e4f44da21
MuhammedNime <jamalnama19@gmail.com>
2022-02-11 16:24:04 +0300

    project

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace console_program
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string ad, soyad;
+            Console.WriteLine("Hello World!");
+            Console.Write("adinizi giriniz: ");
+            ad = Console.ReadLine();
+            Console.Write("soyadinizi giriniz: ");
+            soyad = Console.ReadLine();
+            Console.WriteLine("ad soyad: " + ad + " " + soyad);
+  
+        }
+    }
+}
